9659f0d06b6ad41181ea2a47c30fce43676c4978
Monica Siegel <mhsiegel@umich.edu>
2018-04-10 09:59:53 -0400

    post and get

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
-import requests
-import json
+from flask import Flask, render_template, request
+# import requests
+# import json
 import model
 
 app = Flask(__name__)
@@ -14,10 +14,33 @@ def index():
             <li><a href="/bball"> Men's Basketball </a></li>
         </ul>
     '''
+#this is example without POST!!!
+# @app.route('/bball')
+# def bball():
+#     return render_template("seasons.html", seasons=model.get_bball_seasons())
 
-@app.route('/bball')
+@app.route('/bball',  methods = ['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+    return render_template("seasons.html", seasons=seasons)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        firstname = ''
+        lastname = ''
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
+
+# @app.route('/')
 
 if __name__ == '__main__':
+    model.init_bball()
     app.run(debug=True)
--- model.py
+++ model.py
@@ -5,9 +5,36 @@ BB_FILE_NAME = 'umbball.csv'
 
 bb_seasons = []
 
-def init_bball(csv_file_name):
-    pass
+def init_bball(csv_file_name = BB_FILE_NAME):
+    global bb_seasons
+    r = open(csv_file_name, 'r')
+    read = csv.reader(r)
+    next(read)
+    next(read)
+    bb_seasons = []
+    for r in read:
+        r[3] = int(r[3])
+        r[4] = int(r[4])
+        r[5] = float(r[5])
+        bb_seasons.append(r)
+
+
+#this was the first example!!! without post!!!
+# def get_bball_seasons(sortby='year', sortorder='desc'):
+#     global bb_seasons
+#     return bb_seasons
+
 
 def get_bball_seasons(sortby='year', sortorder='desc'):
-    global bb_seasons
-    return bb_seasons
+    if sortby == 'year':
+        sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(bb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
